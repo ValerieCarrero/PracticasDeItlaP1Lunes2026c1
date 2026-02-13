6e55a9c767b1d8656010cdcce7c6b7a7afb6cd3f
Valerie Carrero <valeriecarrero3@gmail.com>
2026-02-13 11:27:37 -0500

    Add Par or Odd Validator project

---
+++ SecondProject/SecondProject/SecondProject/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int TypedNumber = 0;
+bool IsValidNumber = false;
+
+Console.WriteLine("Par or Odd Number Validator");
+Console.WriteLine("------------------------------");
+
+while (!IsValidNumber)
+{
+    Console.WriteLine("Enter a number: ");
+    try
+    {
+        TypedNumber = Convert.ToInt32(Console.ReadLine());
+        IsValidNumber = true;
+
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid input. Please enter a valid integer.");
+    }
+
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number is too large or too small");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Unexpected error: {ex.Message}");
+    }
+
+}
+
+if (TypedNumber % 2 == 0)
+{
+    Console.WriteLine($"The number {TypedNumber} is even.");
+}
+else
+{
+    Console.WriteLine($"The number {TypedNumber} is odd.");
+}
